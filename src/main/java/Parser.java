40bd45508a913229faab6e549918d6c74c4812f0
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-15 16:28:30 +0300

    Scripted auto-commit on change (2019-10-15 16:28:30) by gitwatch.sh

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -3,10 +3,10 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
-java.io.IO
+import java.io.IOException;
 
 public class Parser {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File csvData = new File("/path/to/csv");
         CSVParser parser = CSVParser.parse(csvData, StandardCharsets.UTF_8, CSVFormat.RFC4180);
         for (CSVRecord csvRecord : parser) {
