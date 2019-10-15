57c6b0f3c4ad3173a107046c61a51bb8cd1fe402
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-15 16:20:43 +0300

    Scripted auto-commit on change (2019-10-15 16:20:43) by gitwatch.sh

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,8 +1,11 @@
-public class Parser {
-    
-    File csvData = new File("/path/to/csv");
-    CSVParser parser = CSVParser.parse(csvData, CSVFormat.RFC4180);
-    for (CSVRecord csvRecord : parser) {
 
+
+public class Parser {
+    public static void main(String[] args) {
+        File csvData = new File("/path/to/csv");
+        CSVParser parser = CSVParser.parse(csvData, CSVFormat.RFC4180);
+        for (CSVRecord csvRecord : parser) {
+            System.out.println(csvRecord);
+        }
     }
 }
