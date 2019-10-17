9d5f4dc30dc0c8fde078bdc0149cc723ef5d734f
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-18 00:59:54 +0300

    Scripted auto-commit on change (2019-10-18 00:59:54) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -1,2 +1,12 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.apache.commons.lang3.StringUtils;
+
 public class AirportMapper {
+    
 }
