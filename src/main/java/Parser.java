167938b4e8d209a5eb93766106b8c45ebadb9322
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-18 00:56:33 +0300

    Scripted auto-commit on change (2019-10-18 00:56:33) by gitwatch.sh

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -12,7 +12,10 @@ import java.nio.file.Paths;
 public class Parser {
     public static void main(String[] args) throws IOException {
         String content = new String(Files.readAllBytes(Paths.get("src/main/resources/L_AIRPORT_ID.csv")));
-        CSVParser parser = CSVParser.parse(content, StandardCharsets.UTF_8, CSVFormat.RFC4180);
+        CSVParser parser = CSVParser.parse(content, CSVFormat.RFC4180);
+        for (CSVRecord csvRecord : parser) {
+            System.out.println(csvRecord);
+        }
 
 //        Reader in = new FileReader("src/main/resources/L_AIRPORT_ID.csv");
 //        Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(in);
