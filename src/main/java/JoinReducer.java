a37902986d1d71251ed9428f578e7de203dc2694
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-18 01:26:35 +0300

    Scripted auto-commit on change (2019-10-18 01:26:35) by gitwatch.sh

--- src/main/java/JoinReducer.java
+++ src/main/java/JoinReducer.java
@@ -1,3 +1,11 @@
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.apache.hadoop.mapreduce.Reducer;
+
 public class JoinReducer {
     
 }
