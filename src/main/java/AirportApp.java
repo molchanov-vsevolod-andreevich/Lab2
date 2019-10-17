e1437de0377a8e8b0a515871c9f961e358e049dc
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-18 01:54:54 +0300

    Scripted auto-commit on change (2019-10-18 01:54:54) by gitwatch.sh

--- src/main/java/AirportApp.java
+++ src/main/java/AirportApp.java
@@ -1,2 +1,10 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 public class AirportApp {
+    
 }
