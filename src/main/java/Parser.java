ca6e0ce19803ef32c96a54c329b4b885c433bca5
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-18 01:13:27 +0300

    Scripted auto-commit on change (2019-10-18 01:13:27) by gitwatch.sh

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -11,7 +11,7 @@ import java.nio.file.Paths;
 
 public class Parser {
     public static void main(String[] args) throws IOException {
-        String s = "-32.00";
+        String s = "7.00";
         float f = Float.parseFloat(s);
         System.out.println(f);
 
