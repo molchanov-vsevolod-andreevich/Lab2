a290ece7e6c7dd0bc37f63ed887712c318ab2ce7
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-17 22:32:43 +0300

    Scripted auto-commit on change (2019-10-17 22:32:43) by gitwatch.sh

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -10,9 +10,12 @@ public class Parser {
     public static void main(String[] args) throws IOException {
         File csvData = new File("src/main/resources/L_AIRPORT_ID.csv");
         CSVParser parser = CSVParser.parse(csvData, StandardCharsets.UTF_8, CSVFormat.RFC4180);
-        List<CSVRecord> m = parser.getRecords();
-        for (CSVRecord csvRecord : parser) {
-            System.out.println(csvRecord.get(1));
+        List<CSVRecord> l = parser.getRecords();
+//        for (CSVRecord csvRecord : parser) {
+//            System.out.println(csvRecord.get(1));
+//        }
+        for (CSVRecord r : l) {
+            System.out.println();
         }
     }
 }
