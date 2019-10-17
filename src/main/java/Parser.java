95c746eedfbea42edbfa46deaf39547ec3dade97
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-18 01:17:16 +0300

    Scripted auto-commit on change (2019-10-18 01:17:16) by gitwatch.sh

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -16,7 +16,7 @@ public class Parser {
 //        System.out.println(f);
 
         String content = new String(Files.readAllBytes(Paths.get("src/main/resources/664600583_T_ONTIME_sample.csv")));
-        CSVParser parser = CSVParser.parse(content, CSVFormat.RFC4180);
+        CSVParser parser = CSVParser.parse(content, CSVFormat.RFC4180.withHeader());
         for (CSVRecord csvRecord : parser) {
             System.out.println(csvRecord);
         }
