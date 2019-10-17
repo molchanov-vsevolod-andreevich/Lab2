6f5bc00f2ffcd4040e27e47cddffa7a7c890b657
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-18 02:00:10 +0300

    Scripted auto-commit on change (2019-10-18 02:00:10) by gitwatch.sh

--- src/main/java/AirportApp.java
+++ src/main/java/AirportApp.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class AirportApp {
@@ -16,8 +17,8 @@ public class AirportApp {
         Job job = Job.getInstance();
         job.setJarByClass(AirportApp.class);
         job.setJobName("Airports");
-        MultipleInputs.addInputPath(job, new Path(args[0]), );
-        MultipleInputs.addInputPath(job, new Path(args[1]));
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, );
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, );
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
         job.setReducerClass(JoinReducer.class);
         job.setOutputKeyClass(Text.class);
