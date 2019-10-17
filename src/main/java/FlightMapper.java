08750980beec458a2bc9f37d40da62f29ec13629
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-18 01:31:52 +0300

    Scripted auto-commit on change (2019-10-18 01:31:52) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -4,7 +4,6 @@ import java.util.StringTokenizer;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
-import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Mapper;
