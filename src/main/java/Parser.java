3fbf48e8662150ae01d0049abbe30975571043db
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-17 23:09:54 +0300

    Scripted auto-commit on change (2019-10-17 23:09:54) by gitwatch.sh

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -4,19 +4,20 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.io.IOException;
-import java.util.List;
+//import java.util.List;
 
 public class Parser {
     public static void main(String[] args) throws IOException {
         File csvData = new File("src/main/resources/L_AIRPORT_ID.csv");
         CSVParser parser = CSVParser.parse(csvData, StandardCharsets.UTF_8, CSVFormat.RFC4180);
-        List<CSVRecord> l = parser.getRecords();
+//        List<CSVRecord> l = parser.getRecords();
 //        for (CSVRecord csvRecord : parser) {
+        System.out.println(parser);
 //            System.out.println(csvRecord.get(1));
 //        }
-        for (CSVRecord r : l) {
-            String s = r.toString();
-            System.out.println(s);
-        }
+//        for (CSVRecord r : l) {
+//            String s = r.toString();
+//            System.out.println(s);
+//        }
     }
 }
