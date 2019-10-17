e3a21e7441a77143e425162190740f3bb43c673b
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-18 00:59:36 +0300

    Scripted auto-commit on change (2019-10-18 00:59:36) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -1,2 +1,11 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.apache.commons.lang3.StringUtils;
+
 public class FlightMapper {
 }
