bdee9eb510742608045c31d72ef5be80fc01bfd1
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-17 22:40:50 +0300

    Scripted auto-commit on change (2019-10-17 22:40:50) by gitwatch.sh

--- src/main/java/CsvWritable.java
+++ src/main/java/CsvWritable.java
@@ -4,6 +4,13 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
 public class CsvWritable implements Writable {
     private int counter;
     private long timestamp;
