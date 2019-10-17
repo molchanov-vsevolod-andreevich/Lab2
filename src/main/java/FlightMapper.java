fe37182176ae86f328bbf655f815f19c8741565a
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-18 01:10:22 +0300

    Scripted auto-commit on change (2019-10-18 01:10:22) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -1,5 +1,9 @@
 import java.io.IOException;
 import java.util.StringTokenizer;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.LongWritable;
@@ -8,4 +12,11 @@ import org.apache.hadoop.mapreduce.Mapper.Context;
 import org.apache.commons.lang3.StringUtils;
 
 public class FlightMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        CSVParser parser = CSVParser.parse(value.toString(), CSVFormat.RFC4180.withHeader());
+        for (CSVRecord csvRecord : parser) {
+            context.write(new Text(csvRecord.get(0)), new Text("airport    " + csvRecord.get(1)));
+        }
+    }
 }
