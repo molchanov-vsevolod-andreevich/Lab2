47811a3ba8bfda8314cc7d86b284fa964451f388
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-18 19:21:43 +0300

    Scripted auto-commit on change (2019-10-18 19:21:43) by gitwatch.sh

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -11,7 +11,7 @@ import java.nio.file.Paths;
 
 public class Parser {
     public static void main(String[] args) throws IOException {
-        String s1 = "10000";
+        String s1 = "10001";
         String s2 = "10001";
         System.out.println(s1.compareTo(s2));
 
