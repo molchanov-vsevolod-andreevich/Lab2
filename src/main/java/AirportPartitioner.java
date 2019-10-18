651731fbb6492337dc6f4831195348528e7f825e
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-18 22:56:44 +0300

    Scripted auto-commit on change (2019-10-18 22:56:44) by gitwatch.sh

--- src/main/java/AirportPartitioner.java
+++ src/main/java/AirportPartitioner.java
@@ -4,10 +4,10 @@ import org.apache.hadoop.mapreduce.Partitioner;
 public class AirportPartitioner extends Partitioner<AirportPair, Text> {
     @Override
     public int getPartition(AirportPair key, Text value, int numReduceTasks) {
+        System.out.println(key.getAirportID());
         if (key.getAirportID().compareTo(new Text("3343")) == -1) {
             return 0;
         } else {
-            System.out.println(key.getAirportID());
             return 1;
         }
     }
