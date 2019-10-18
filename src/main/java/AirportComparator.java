f1cc53bc7e68b9ce7da3ccd8e022b655373d28fc
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-18 21:45:32 +0300

    Scripted auto-commit on change (2019-10-18 21:45:32) by gitwatch.sh

--- src/main/java/AirportComparator.java
+++ src/main/java/AirportComparator.java
@@ -1,2 +1,15 @@
-public class AirportComparator {
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+public class AirportComparator extends WritableComparator {
+//        protected AirportComparator() {
+//            super((Class<? extends WritableComparable>) AirportComparator.class, true);
+//        }
+
+    @Override
+    public int compare(WritableComparable o1, WritableComparable o2) {
+        AirportPair airport1 = (AirportPair) o1;
+        AirportPair airport2 = (AirportPair) o2;
+        return airport1.AIRPORT_ID.compareTo(airport2.AIRPORT_ID);
+    }
 }
