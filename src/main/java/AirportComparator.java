c0fd56d240bd391e5e37d9054ecb36df1c50df54
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-18 23:14:07 +0300

    Scripted auto-commit on change (2019-10-18 23:14:07) by gitwatch.sh

--- src/main/java/AirportComparator.java
+++ src/main/java/AirportComparator.java
@@ -10,7 +10,7 @@ public class AirportComparator extends WritableComparator {
     public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
         Integer i1 = readInt(b1, s1);
         Integer i2 = readInt(b2, s2);
-        return 
+        return i1.compareTo(i2);
     }
 
     @Override
