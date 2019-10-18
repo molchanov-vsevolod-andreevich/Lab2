8dc9c263bad19cc10cfcb5effdd890ba53c93be9
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-18 23:33:42 +0300

    Scripted auto-commit on change (2019-10-18 23:33:42) by gitwatch.sh

--- src/main/java/AirportComparator.java
+++ src/main/java/AirportComparator.java
@@ -15,10 +15,10 @@ public class AirportComparator extends WritableComparator {
 
     @Override
     public int compare(WritableComparable o1, WritableComparable o2) {
-//        AirportPair airport1 = (AirportPair) o1;
-//        AirportPair airport2 = (AirportPair) o2;
-//        System.out.println(airport1.getAirportID() + " : " + airport2.getAirportID() + " => " + airport1.getAirportID().compareTo(airport2.getAirportID()));
-//        return airport1.getAirportID().compareTo(airport2.getAirportID());
-        return o1.compareTo(o2);
+        AirportPair airport1 = (AirportPair) o1;
+        AirportPair airport2 = (AirportPair) o2;
+        System.out.println(airport1.getAirportID() + " : " + airport2.getAirportID() + " => " + airport1.getAirportID().compareTo(airport2.getAirportID()));
+        return airport1.getAirportID().compareTo(airport2.getAirportID());
+//        return o1.compareTo(o2);
     }
 }
