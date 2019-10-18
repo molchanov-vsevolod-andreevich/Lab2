9a01bed8fea8ea5823d4c329ca27f0c901e91f51
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-18 21:49:10 +0300

    Scripted auto-commit on change (2019-10-18 21:49:10) by gitwatch.sh

--- src/main/java/AirportPair.java
+++ src/main/java/AirportPair.java
@@ -59,4 +59,17 @@ public class AirportPair implements WritableComparable<AirportPair> {
         }
         return res;
     }
+
+    public class AirportComparator extends WritableComparator {
+        protected AirportComparator() {
+            super((Class<? extends WritableComparable>) AirportComparator.class, true);
+        }
+
+        @Override
+        public int compare(WritableComparable o1, WritableComparable o2) {
+            AirportPair airport1 = (AirportPair) o1;
+            AirportPair airport2 = (AirportPair) o2;
+            return airport1.getAirportID().compareTo(airport2.getAirportID());
+        }
+    }
 }
