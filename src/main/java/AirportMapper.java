e5c41fd1434e5e41e2f3b36fe215b3c0cfe3991b
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-18 13:01:42 +0300

    Scripted auto-commit on change (2019-10-18 13:01:42) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -12,12 +12,7 @@ public class AirportMapper extends Mapper<LongWritable, Text, Text, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         CSVParser parser = CSVParser.parse(value.toString(), CSVFormat.RFC4180);
-        boolean i = true;
         for (CSVRecord csvRecord : parser) {
-            if (i) {
-                i = false;
-                continue;
-            }
             context.write(new Text(csvRecord.get(0)), new Text("airport:" + csvRecord.get(1)));
         }
     }
--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -13,12 +13,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, Text, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         CSVParser parser = CSVParser.parse(value.toString(), CSVFormat.RFC4180);
-        boolean i = true;
         for (CSVRecord csvRecord : parser) {
-            if (i) {
-                i = false;
-                continue;
-            }
             String del = csvRecord.get(18);
             if (!del.equals("")) {
                 context.write(new Text(csvRecord.get(14)), new Text("delay:" + del));
