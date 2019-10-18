cfad94c8e5b7e8e3f246023fe02048de33240191
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-19 00:30:14 +0300

    Scripted auto-commit on change (2019-10-19 00:30:14) by gitwatch.sh

--- src/main/java/AirportPair.java
+++ src/main/java/AirportPair.java
@@ -56,12 +56,6 @@ public class AirportPair implements WritableComparable<AirportPair> {
 
     @Override
     public int compareTo(AirportPair o) {
-//        int res = INSERT_ID.compareTo(o.INSERT_ID);
-//        if (res == 0) {
-//            res = AIRPORT_ID.compareTo(o.AIRPORT_ID);
-//        }
-//        return res;
-
         int res = AIRPORT_ID.compareTo(o.AIRPORT_ID);
         if (res == 0) {
             res = INSERT_ID.compareTo(o.INSERT_ID);
@@ -69,27 +63,27 @@ public class AirportPair implements WritableComparable<AirportPair> {
         return res;
     }
 
-//    public class AirportPartitioner extends Partitioner<AirportPair, Text> {
-//        @Override
-//        public int getPartition(AirportPair key, Text value, int numReduceTasks) {
-//            if (key.getAirportID().compareTo(new Text("3343")) == -1) {
-//                return 0;
-//            } else {
-//                return 1;
-//            }
-//        }
-//    }
+    public class AirportPartitioner extends Partitioner<AirportPair, Text> {
+        @Override
+        public int getPartition(AirportPair key, Text value, int numReduceTasks) {
+            if (Integer.parseInt(key.getAirportID().toString()) < 13344) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+    }
 
-//    public class AirportComparator extends WritableComparator {
-//        protected AirportComparator() {
-//            super(AirportPair.class, true);
-//        }
-//
-//        @Override
-//        public int compare(WritableComparable o1, WritableComparable o2) {
-//            AirportPair airport1 = (AirportPair) o1;
-//            AirportPair airport2 = (AirportPair) o2;
-//            return airport1.getAirportID().compareTo(airport2.getAirportID());
-//        }
-//    }
+    public class AirportComparator extends WritableComparator {
+        protected AirportComparator() {
+            super(AirportPair.class, true);
+        }
+
+        @Override
+        public int compare(WritableComparable o1, WritableComparable o2) {
+            AirportPair airport1 = (AirportPair) o1;
+            AirportPair airport2 = (AirportPair) o2;
+            return airport1.getAirportID().compareTo(airport2.getAirportID());
+        }
+    }
 }
