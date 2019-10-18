db2caf4d062aeec8c3118fb0ec281ea1b762e2ea
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-18 04:23:24 +0300

    Scripted auto-commit on change (2019-10-18 04:23:24) by gitwatch.sh

--- src/main/java/AirportApp.java
+++ src/main/java/AirportApp.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.conf.Configuration;
 
 public class AirportApp {
     public static void main(String[] args) throws Exception {
