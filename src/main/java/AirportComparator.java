2fed34681602be07ec7106e2215ea66f8a659016
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-18 23:36:49 +0300

    Scripted auto-commit on change (2019-10-18 23:36:49) by gitwatch.sh

--- src/main/java/AirportComparator.java
+++ src/main/java/AirportComparator.java
@@ -1,24 +1,26 @@
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
-public class AirportComparator extends WritableComparator {
-    protected AirportComparator() {
-        super(AirportPair.class, true);
-    }
+public class AirportComparator 
 
-//    @Override
-//    public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
-//        Integer i1 = readInt(b1, s1);
-//        Integer i2 = readInt(b2, s2);
-//        return i1.compareTo(i2);
+//public class AirportComparator extends WritableComparator {
+//    protected AirportComparator() {
+//        super(AirportPair.class, true);
 //    }
-
-    @Override
-    public int compare(WritableComparable o1, WritableComparable o2) {
-        AirportPair airport1 = (AirportPair) o1;
-        AirportPair airport2 = (AirportPair) o2;
-        System.out.println(airport1.getAirportID() + " : " + airport2.getAirportID() + " => " + airport1.getAirportID().compareTo(airport2.getAirportID()));
-        return airport1.getAirportID().compareTo(airport2.getAirportID());
-//        return o1.compareTo(o2);
-    }
-}
+//
+////    @Override
+////    public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+////        Integer i1 = readInt(b1, s1);
+////        Integer i2 = readInt(b2, s2);
+////        return i1.compareTo(i2);
+////    }
+//
+//    @Override
+//    public int compare(WritableComparable o1, WritableComparable o2) {
+//        AirportPair airport1 = (AirportPair) o1;
+//        AirportPair airport2 = (AirportPair) o2;
+//        System.out.println(airport1.getAirportID() + " : " + airport2.getAirportID() + " => " + airport1.getAirportID().compareTo(airport2.getAirportID()));
+//        return airport1.getAirportID().compareTo(airport2.getAirportID());
+////        return o1.compareTo(o2);
+//    }
+//}
