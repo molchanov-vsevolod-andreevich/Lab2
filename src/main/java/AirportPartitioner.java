199c1ab8f0525dfbe0764127fa0bbca702c70990
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-18 20:22:50 +0300

    Scripted auto-commit on change (2019-10-18 20:22:50) by gitwatch.sh

--- src/main/java/AirportPartitioner.java
+++ src/main/java/AirportPartitioner.java
@@ -4,10 +4,10 @@ import org.apache.hadoop.mapreduce.Partitioner;
 public class AirportPartitioner extends Partitioner<AirportPair, Text> {
     @Override
     public int getPartition(AirportPair key, Text value, int numReduceTasks) {
-        if () {
+        if (key.getAirportID().compareTo()) {
 
         } else {
-            return 
+            return
         }
     }
 }
