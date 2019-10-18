60bd9d1e5c291d081bc7b35f69298b01f7fcda31
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-19 00:58:05 +0300

    Scripted auto-commit on change (2019-10-19 00:58:05) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -12,7 +12,6 @@ public class AirportMapper extends Mapper<LongWritable, Text, AirportPair, Text>
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         CSVParser parser = CSVParser.parse(value.toString(), CSVFormat.RFC4180);
         for (CSVRecord csvRecord : parser) {
-//            System.out.println(csvRecord.get(0) + " " + "0 Name:" + csvRecord.get(1));
             context.write(new AirportPair(csvRecord.get(0), 0), new Text(csvRecord.get(1)));
         }
     }
