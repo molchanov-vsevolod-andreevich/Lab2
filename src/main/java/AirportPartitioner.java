115a8ea7cd1dcf8c2c6cd0bb4fa86df9cf680557
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-18 22:58:16 +0300

    Scripted auto-commit on change (2019-10-18 22:58:16) by gitwatch.sh

--- src/main/java/AirportPartitioner.java
+++ src/main/java/AirportPartitioner.java
@@ -5,7 +5,7 @@ public class AirportPartitioner extends Partitioner<AirportPair, Text> {
     @Override
     public int getPartition(AirportPair key, Text value, int numReduceTasks) {
         System.out.println(key.getAirportID());
-        if (key.getAirportID().compareTo(new Text("133343")) == -1) {
+        if (key.getAirportID().compareTo(new Text("13344")) == -1) {
             return 0;
         } else {
             return 1;
