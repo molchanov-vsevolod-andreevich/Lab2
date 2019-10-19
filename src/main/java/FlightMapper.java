4e1715c370f71f4ae9f2f543ad19f90d378e8fb4
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-19 12:01:34 +0300

    Scripted auto-commit on change (2019-10-19 12:01:34) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -8,6 +8,9 @@ import org.apache.hadoop.mapreduce.Mapper;
 public class FlightMapper extends Mapper<LongWritable, Text, AirportPair, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        if (key.get() == 0) { // header CSV
+            return;
+        }
         CSVRecord csvRecord = Util.getCsvRecord(value);
         String delay = csvRecord.get(18);
         if (!delay.equals("")) {
