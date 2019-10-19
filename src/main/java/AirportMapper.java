aa8a3c6ba744cd39da6b5170003c632963cf898f
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-19 10:50:58 +0300

    Scripted auto-commit on change (2019-10-19 10:50:58) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -10,9 +10,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 public class AirportMapper extends Mapper<LongWritable, Text, AirportPair, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        if (key.equals(0)) {
-            System.out.println("YYYYYYYEEEEEEESSSSSSSSSS");
-        }
+        System.out.println(key);
         CSVParser parser = CSVParser.parse(value.toString(), CSVFormat.RFC4180);
         for (CSVRecord csvRecord : parser) {
             context.write(new AirportPair(csvRecord.get(0), 0), new Text(csvRecord.get(1)));
