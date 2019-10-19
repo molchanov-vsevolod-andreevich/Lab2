8060b5ac6ac60b467c626a43576172fab11a2958
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-19 11:19:36 +0300

    Scripted auto-commit on change (2019-10-19 11:19:36) by gitwatch.sh

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -8,6 +8,6 @@ import java.io.IOException;
 
 public abstract class Util {
     public static final CSVRecord getCsvRecord(Text line) throws IOException {
-        return CSVParser.parse(line.toString(), CSVFormat.RFC4180);
+        return CSVParser.parse(line.toString(), CSVFormat.RFC4180).getRecords().get(0);
     }
 }
