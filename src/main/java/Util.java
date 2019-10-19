bdeb7c402b7c53a4fe8d5314d03a1e8f7f7af05c
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-19 11:03:42 +0300

    Scripted auto-commit on change (2019-10-19 11:03:42) by gitwatch.sh

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,7 +1,8 @@
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 
 public abstract class Util {
-    public static final parseCvsWithHeader (LongWritable key) {
+    public static final parseCvsWithHeader (LongWritable lineNumber, Text value) {
 
     }
 }
