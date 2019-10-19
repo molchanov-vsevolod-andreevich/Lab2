58cdc098b57d3a7d8344e1725bc0441041ddc34e
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-19 11:09:20 +0300

    Scripted auto-commit on change (2019-10-19 11:09:20) by gitwatch.sh

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,8 +1,13 @@
+import org.apache.commons.csv.CSVRecord;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 
 public abstract class Util {
-    public static final parseCvsWithHeader (LongWritable lineNumber, Text value) {
+    public static final CSVRecord parseCvsWithHeader (LongWritable lineNumber, Text line) {
+        if (lineNumber == 0) {
+            return CSVRecord("header");
+        } else {
 
+        }
     }
 }
