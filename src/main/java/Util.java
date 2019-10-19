86bcd53e703dcbc1e2c7cf0490c8e9ff33c36fe6
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-19 11:18:11 +0300

    Scripted auto-commit on change (2019-10-19 11:18:11) by gitwatch.sh

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,13 +1,11 @@
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 
 public abstract class Util {
     public static final CSVRecord getCsvRecord(Text line) {
-        if (lineNumber.get() == 0) {
-            return
-        } else {
-
-        }
+        return CSVParser parser = CSVParser.parse(value.toString(), CSVFormat.RFC4180);
     }
 }
