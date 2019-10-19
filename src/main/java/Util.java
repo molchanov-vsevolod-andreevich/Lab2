fca7f5f322701fc5551cbb5d54317d42a3ba213e
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-10-19 11:14:29 +0300

    Scripted auto-commit on change (2019-10-19 11:14:29) by gitwatch.sh

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -3,6 +3,8 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 
 public abstract class Util {
+    public static final boolean contains
+
     public static final String getCsvRecordExcludeHeader (LongWritable lineNumber, Text line) {
         if (lineNumber.get() == 0) {
             return
